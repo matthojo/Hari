b0b8493ce4110710daf7b20584c6b58c068f395f
Matthew Harrison-Jones <matthojo@gmail.com>
2011-12-29 11:00:43 +0000

    Removed Twitter and Google Plus buttons for now
    They don't support # URLs

--- classes/model/images.class.php
+++ classes/model/images.class.php
@@ -91,9 +91,9 @@ class Images {
 	    	    	<span class="marker"></span>
 	    	    </span>
 	    	    <div class="social">
-	    	    	        <div class="social-item"><div class="g-plusone" data-size="tall" data-href="'.URL.'#'.$name.'"></div></div>
+	    	    	        <!-- <div class="social-item"><div class="g-plusone" data-size="tall" data-href="'.URL.'#'.$name.'"></div></div>
 	    	    	        <div class="social-item"><a href="https://twitter.com/share" class="twitter-share-button" data-url="'.URL.'#'.$name.'" data-count="vertical">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div> -->
                             <div class="social-item"><div class="fb-like" data-href="'.URL.'#'.$name.'" data-send="false" data-layout="box_count" data-width="55px" data-show-faces="false" data-font="arial"></div></div>
 
 	    	    </div>
